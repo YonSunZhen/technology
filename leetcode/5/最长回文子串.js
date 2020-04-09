e050276ae17ee8fc08cc476d03a6198539ee4f04
Sun Yongzhen <1315976441@qq.com>
2020-04-09 20:43:33 +0800

    leetcode: add 三数之和

--- leetcode/5/最长回文子串.js
+++ leetcode/5/最长回文子串.js
@@ -1,22 +1,23 @@
 // 回文
 // 动态规划
-function longestPalindrome(s) {
-  if(s.length === 0) {
+// 有问题
+function longestPalindrome (s) {
+  if (s.length === 0) {
     return "";
   }
   let reverseStr = s.split("").reverse().join("");
   let result = s[0]; // 字符串切记初始化 不然为undefined
-  for(let i = 0; i < s.length; i++) {  
+  for (let i = 0; i < s.length; i++) {
     let str = s[i];
-    for(let j = i + 1; j < s.length; j++) {
+    for (let j = i + 1; j < s.length; j++) {
       str += s[j];
       // 验证正序和反序是否一样
-      let _reverseStr = reverseStr.slice(i, j+1);
+      let _reverseStr = reverseStr.slice(i, j + 1);
       console.log(_reverseStr);
-      
+
       // 这里也有n 因此空间复杂度是n的3次方
-      if(str === _reverseStr) {
-        if(str.length > result.length) {
+      if (str === _reverseStr) {
+        if (str.length > result.length) {
           result = str;
         }
       }
