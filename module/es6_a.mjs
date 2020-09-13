50520e3e00fd20502fb21afc1e979eeee30e5076
YonSunZhen <1315976441@qq.com>
2020-09-13 19:09:06 +0800

    module: update es6 module

--- module/es6_a.mjs
+++ module/es6_a.mjs
@@ -4,12 +4,9 @@ setTimeout(() => {
   aNum = 11;
   aArr.push(11);
   console.log('1s a模块改变数据aNum&aArr:');
-  console.log(`aNum=${aNum},`);
-  console.log(`aArr=${aArr},`);
+  console.log(`aNum=${aNum}`);
+  console.log(`aArr=${aArr}`);
 }, 1000)
-setTimeout(() => {
-  console.log(`3s a模块接受数据:aNum=,aArr=`);
-}, 3000);
 // 这种写法 无论什么类型 一律值引用内部变 外部也变
 // export {
 //   aNum,
@@ -19,7 +16,8 @@ setTimeout(() => {
 // 这种写法 按照传值类型来 与其他的不太一样
 // 与module.exports类似
 export default {
-  aNum
+  aNum, // 数值类型 深拷贝
+  aArr // 对象类型 浅拷贝
 }
-console.log(`a模块已导出数据:aNum=,aArr=`);
+console.log(`a模块已导出数据:aNum=${aNum},aArr=${aArr}`);
 
--- module/es6_b.mjs
+++ module/es6_b.mjs
@@ -4,11 +4,5 @@ console.log(`b模块已接收数据:aNum=${a.aNum},aArr=${a.aArr}`);
 // console.log(`b模块已接收数据:aNum=${aNum},aArr=${aArr}`);
 setTimeout(() => {
   // console.log(`2s b模块接受数据:aNum=${aNum},aArr=${aArr}`);
-  // aNum = 2;
-  // aArr = [2]
-  // console.log(`2s b模块改变数据:aNum=${aNum},aArr=${aArr}`);
   console.log(`2s b模块接受数据:aNum=${a.aNum},aArr=${a.aArr}`);
-  a.aNum = 2;
-  a.aArr = [2]
-  console.log(`2s b模块改变数据:aNum=${a.aNum},aArr=${a.aArr}`);
 }, 2000);
