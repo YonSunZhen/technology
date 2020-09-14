69ff4c95868008cd16157fda7e80bb657cb1b6a1
YonSunZhen <1315976441@qq.com>
2020-09-14 11:57:03 +0800

    module: update es6 module

--- module/es6_a.mjs
+++ module/es6_a.mjs
@@ -1,23 +1,33 @@
-let aNum = 1;
-let aArr = [1];
-setTimeout(() => {
-  aNum = 11;
-  aArr.push(11);
-  console.log('1s a模块改变数据aNum&aArr:');
-  console.log(`aNum=${aNum}`);
-  console.log(`aArr=${aArr}`);
-}, 1000)
-// 这种写法 无论什么类型 一律值引用内部变 外部也变
-// export {
-//   aNum,
-//   aArr
+// let aNum = 1;
+// let aArr = [1];
+// setTimeout(() => {
+//   aNum = 11;
+//   aArr.push(11);
+//   console.log('1s a模块改变数据aNum&aArr:');
+//   console.log(`aNum=${aNum}`);
+//   console.log(`aArr=${aArr}`);
+// }, 1000)
+// // 这种写法 无论什么类型 一律值引用内部变 外部也变
+// // export {
+// //   aNum,
+// //   aArr
+// // }
+
+// // 这种写法 按照传值类型来 与其他的不太一样
+// // 与module.exports类似
+// export default {
+//   aNum, // 数值类型 深拷贝
+//   aArr // 对象类型 浅拷贝
+// }
+// console.log(`a模块已导出数据:aNum=${aNum},aArr=${aArr}`);
+
+// export let color = 'red';
+// export default function(num1, num2) {
+//   return num1 + num2;
 // }
 
-// 这种写法 按照传值类型来 与其他的不太一样
-// 与module.exports类似
-export default {
-  aNum, // 数值类型 深拷贝
-  aArr // 对象类型 浅拷贝
+function sum(num1, num2) {
+  return num1 + num2;
 }
-console.log(`a模块已导出数据:aNum=${aNum},aArr=${aArr}`);
+export { sum as a };
 
--- module/es6_b.mjs
+++ module/es6_b.mjs
@@ -1,8 +1,16 @@
-import a from './es6_a.mjs';
-// import {aNum, aArr} from './es6_a.mjs';
-console.log(`b模块已接收数据:aNum=${a.aNum},aArr=${a.aArr}`);
-// console.log(`b模块已接收数据:aNum=${aNum},aArr=${aArr}`);
-setTimeout(() => {
-  // console.log(`2s b模块接受数据:aNum=${aNum},aArr=${aArr}`);
-  console.log(`2s b模块接受数据:aNum=${a.aNum},aArr=${a.aArr}`);
-}, 2000);
+// import a from './es6_a.mjs';
+// // import {aNum, aArr} from './es6_a.mjs';
+// console.log(`b模块已接收数据:aNum=${a.aNum},aArr=${a.aArr}`);
+// // console.log(`b模块已接收数据:aNum=${aNum},aArr=${aArr}`);
+// setTimeout(() => {
+//   // console.log(`2s b模块接受数据:aNum=${aNum},aArr=${aArr}`);
+//   console.log(`2s b模块接受数据:aNum=${a.aNum},aArr=${a.aArr}`);
+// }, 2000);
+
+// 默认值必须排在非默认值之前
+// import sum, { color } from  './es6_a.mjs';
+// console.log(sum(1,2));
+// console.log(color);
+
+import { a as aa } from './es6_a.mjs';
+console.log(aa(1,2));
